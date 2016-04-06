7d69130bb4a2fc0f544ad2d2f2d97425e51cb426
Rafael Costa <rafael@rafaelcosta.me>
2016-04-06 19:49:41 -0300

    Updated logger to exclude static files

--- logger.js
+++ logger.js
@@ -1,6 +1,7 @@
 var exports = module.exports = {};
 
 exports.requestLogger = function (req, res, next) {
+  if (req.originalUrl.indexOf("/static") != -1) return;
   exports.log(`Received request from '${req.ip}' to page '${req.originalUrl}'`, "INFO");
   next();
 }
